820a67da68b911cc07aaf395cfb63a53720e9894
Jonathan Demaeyer <jodemaey@meteo.be>
2023-07-04 14:36:13 +0200

    Added script to install dependencies

---
+++ install.r
@@ -0,0 +1,3 @@
+install.packages("zoo", repos = "http://cran.us.r-project.org")
+install.packages("crch", repos = "http://cran.us.r-project.org")
+install.packages("ncdf4", repos = "http://cran.us.r-project.org")
